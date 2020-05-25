48acc686eefc66b0a8139ce4a4745058f38e45de
wzh <512105256@qq.com>
2020-05-25 14:55:55 +0800

    单例模式例子

--- src/main/java/com/wzh/singleton/LazySingleton2.java
+++ src/main/java/com/wzh/singleton/LazySingleton2.java
@@ -1,9 +1,12 @@
 package com.wzh.singleton;
 
+import java.io.Serializable;
+
 /**
- * 懒汉式单例模式 - 第一种
+ * 懒汉式单例模式 - 第二种
+ * 外部类加载的时候同时加载内部类，JVM 保证单例
  */
-public class LazySingleton2 {
+public class LazySingleton2 implements Serializable {
 
     private LazySingleton2(){}
 
@@ -15,7 +18,13 @@ public class LazySingleton2 {
     private static class InnerLazy {
         private static final LazySingleton2 instance=new LazySingleton2();
     }
-
+    // 重写readResolve，只不过是覆盖了反序列化出来的对象
+    // 还是创建了两次，发生在 JVM 层面，相对安全
+    // 之前反序列化出的对象会被GC 回收
+    // 防止序列化破坏单例
+    private Object readResolve(){
+        return InnerLazy.instance;
+    }
     public static void main(String[] args) {
         for(int i=0;i<100;i++){
             new Thread(() -> {
