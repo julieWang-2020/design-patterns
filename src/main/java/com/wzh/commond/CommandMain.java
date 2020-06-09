2c2b9f0e9d9c4dd32bf5132710e027a1a03a9c2e
wzh <512105256@qq.com>
2020-06-09 15:13:19 +0800

    chain + command 模式

--- src/main/java/com/wzh/commond/CommandMain.java
+++ src/main/java/com/wzh/commond/CommandMain.java
@@ -1,5 +1,8 @@
 package com.wzh.commond;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author wzh
  * @date 2020-06-08 22:31
@@ -10,19 +13,37 @@ public class CommandMain {
         Content c=new Content();
         c.mag="julie";
         Command insertCommand=new InsertCommand(c);
-        insertCommand.doit();
-        insertCommand.undo();
-        System.out.println(c.mag);
+//        insertCommand.doit();
+//        insertCommand.undo();
+//        System.out.println(c.mag);
 
         Command copyCommand=new CopyCommand(c);
-        copyCommand.doit();
-        copyCommand.undo();
-        System.out.println(c.mag);
+//        copyCommand.doit();
+//        copyCommand.undo();
+//        System.out.println(c.mag);
 
         Command deleteCommand=new DeleteCommand(c);
-        deleteCommand.doit();
-        deleteCommand.undo();
+//        deleteCommand.doit();
+//        deleteCommand.undo();
+//        System.out.println(c.mag);
+
+
+        List<Command> list=new ArrayList<>();
+        list.add(insertCommand);
+        list.add(copyCommand);
+        list.add(deleteCommand);
+
+        for(int i=0;i<list.size();i++){
+            list.get(i).doit();
+        }
+
+        System.out.println("------------------");
+
         System.out.println(c.mag);
 
+        for(int i=list.size();i > 0;i--){
+            list.get(i-1).undo();
+        }
+        System.out.println(c.mag);
     }
 }
